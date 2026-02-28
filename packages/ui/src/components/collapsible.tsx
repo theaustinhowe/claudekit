abac524bc855844b605e361947fd1c52f71f1f74
Austin Howe <ahowe008@gmail.com>
2026-02-28 09:15:24 -0700

    Improve collapsible support

--- packages/ui/src/components/collapsible.tsx
+++ packages/ui/src/components/collapsible.tsx
@@ -3,6 +3,7 @@
 import { Collapsible as CollapsiblePrimitive } from "@base-ui/react/collapsible";
 import * as React from "react";
 import { cn } from "../utils";
+import { Button } from "./button";
 
 const Collapsible = CollapsiblePrimitive.Root;
 
@@ -12,7 +13,8 @@ function CollapsibleTrigger({
   ...props
 }: React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Trigger> & { asChild?: boolean }) {
   if (asChild && React.isValidElement(children)) {
-    const isNativeButton = typeof children.type === "string" && children.type === "button";
+    const isNativeButton =
+      (typeof children.type === "string" && children.type === "button") || children.type === Button;
     return <CollapsiblePrimitive.Trigger render={children} nativeButton={isNativeButton} {...props} />;
   }
   return <CollapsiblePrimitive.Trigger {...props}>{children}</CollapsiblePrimitive.Trigger>;
